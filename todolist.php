4540f699da9cf3632672fb4f793d0d5d499f50c6
jeansc8822 <127043578+jeansc8822@users.noreply.github.com>
2024-05-05 12:14:20 -0400

    Initial commit

---
+++ todolist.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+    <script src="todolist.js"></script>
+</head>
+<body>
+    <?php
+        include('header.php');
+    ?>
+
+    <h2>TO DO LIST</h2>
+    
+</body>
+</html>
